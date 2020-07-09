35f43c2c0b227ff9e6a87b89a19ebefad2500b65
robinsos3623 <67081007+robinsos3623@users.noreply.github.com>
2020-07-08 21:48:53 -0400

    Add files via upload

---
+++ P5T2_FeetToInches_Sinclair Robinson.py
@@ -0,0 +1,23 @@
+# This project converts feet into inches
+# 07-08-2020
+# CTI-110 P5T2_FeetToInches_SinclairRobinson
+# Sinclair Robinson
+
+#Constant for the number of inches per foot.
+INCHES_PER_FOOT = 12
+
+# main function
+def main():
+    # Get a number of feet from the user.
+    feet = int(input('Enter a number of feet:  '))
+
+    # Convert that to inches.
+    print(feet,  'equals', feet_to_inches(feet),  'inches.')
+
+# The feet_to_inches function converts feet to inches.
+def feet_to_inches(feet):
+    return feet * INCHES_PER_FOOT
+
+# Call the main function.
+main()
+
